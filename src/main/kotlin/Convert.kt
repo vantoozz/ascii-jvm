cb695b2d9cbdc1983e9a1daab8f12c22810362c6
Ivan Nikitin <vantoozz@gmail.com>
2020-07-16 17:28:36 -0400

    + width param

--- src/main/kotlin/Convert.kt
+++ src/main/kotlin/Convert.kt
@@ -2,6 +2,10 @@ package com.github.ascii
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.validate
+import com.github.ajalt.clikt.parameters.types.int
 import com.github.ajalt.clikt.parameters.types.path
 import com.sksamuel.scrimage.ImmutableImage
 import com.sksamuel.scrimage.filter.ThresholdFilter
@@ -15,13 +19,17 @@ private val chars = listOf(
     '#', 'M', 'W', '&', '8', '%', 'B', '@', '$'
 )
 
-private const val width = 80
-
 internal class Convert : CliktCommand() {
 
     private val imagePath
             by argument(help = "Image Path")
                 .path(mustExist = true, mustBeReadable = true)
+
+    private val width
+            by option("-w", "--width", help = "Set characters per line")
+                .int().default(80).validate {
+                    require(it > 0) { "Width must be greater than zero" }
+                }
 
     override fun run() {
         ImmutableImage.loader()
